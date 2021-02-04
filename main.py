0805576559d6c2ca1d19802d3c9419093ec37a82
alex <alexalbu78@yahoo.com>
2021-02-04 12:23:25 +0200

    Created results.txt
    Signed-off-by: alex <alexalbu78@yahoo.com>

--- main.py
+++ main.py
@@ -16,18 +16,20 @@ def grade_projects(projects_folder, model):
     grades = {}
     for root, subdirs, files in os.walk(projects_folder):
         for project in subdirs:
-                path = os.path.join(root, project)
-                data = analyze_project(path)
-                grade = model.predict(data)
-                grades[project] = float(grade[0])
+            path = os.path.join(root, project)
+            data = analyze_project(path)
+            grade = model.predict(data)
+            grades[project] = float(grade[0])
         break
     return grades
 
 
-def print_grades(grades):
+def print_grades_to_file(grades, outfile):
     place = 1
+    file = open(outfile, mode="w")
     for key in grades:
-        print(f"Project {key} is on place {place} with grade {grades[key]}.")
+        line = f"{place}. {key} --> {grades[key]}.\n"
+        file.write(line)
         place += 1
 
 
@@ -36,4 +38,4 @@ if __name__ == '__main__':
     grades = grade_projects("test", model)
     # sort the grades in revers
     sorted_grades = {key: val for key, val in sorted(grades.items(), key=lambda ele: ele[1], reverse=True)}
-    print_grades(sorted_grades)
+    print_grades_to_file(sorted_grades, "results.txt")
